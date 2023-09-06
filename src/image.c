005f8d3797a844c892e53d71cd5ae8855dc4bed6
Jan200101 <sentrycraft123@gmail.com>
2023-09-06 11:37:16 +0200

    convert grayscale templates to RGB

--- src/image.c
+++ src/image.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "image.h"
 #include "common.h"
 
@@ -28,7 +30,14 @@ unsigned decode32(uint32_t** out, unsigned* w, unsigned* h,
     png_byte color_type = png_get_color_type(png, info);
 
     if(color_type == PNG_COLOR_TYPE_PALETTE)
+    {
         png_set_palette_to_rgb(png);
+    }
+    else if (color_type == PNG_COLOR_TYPE_GRAY_ALPHA
+            || color_type == PNG_COLOR_TYPE_GRAY)
+    {
+        png_set_gray_to_rgb(png);
+    }
  
     if(png_get_valid(png, info, PNG_INFO_tRNS))
         png_set_tRNS_to_alpha(png);
